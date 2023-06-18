1e2c4459527cc2bca458bed46c54e86a183e338d
Alex McGuire <cage433@gmail.com>
2023-06-19 00:16:50 +0100

    refactor

--- sqlite3_db/tests/test_vortex_sqlite3_db.py
+++ sqlite3_db/tests/test_vortex_sqlite3_db.py
@@ -1,29 +1,60 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Optional
 from unittest import TestCase
 
+from date_range import Day
 from date_range.tests.fixtures import random_day
 from kashflow.invoice import KashflowInvoice
 from sqlite3_db.vortex_sqlite3_db import VortexSqlite3DB
 from testing_utils import RandomisedTest
+from utils import RandomNumberGenerator
 
 
 class VortexSqlite3DBTest(TestCase):
+    def __random_invoice(self, rng: RandomNumberGenerator, paid_date: Optional[Day]) -> KashflowInvoice:
+        return KashflowInvoice(
+            random_day(rng),
+            paid_date=paid_date,
+            reference=rng.choice("test", ""),
+            external_reference=rng.maybe(rng.choice("test", "")),
+            payment=rng.uniform(0, 100),
+            vat=rng.maybe(rng.uniform(0, 100)),
+            invoice_type=rng.choice("Mike", "", None),
+            note=rng.choice("Test invoice", "", None),
+        )
+
     @RandomisedTest(number_of_runs=20)
     def test_db_creation(self, rng):
         with TemporaryDirectory() as tmpdir:
             db = VortexSqlite3DB(Path(tmpdir) / "test.db")
-            invoice = KashflowInvoice(
-                random_day(rng),
-                paid_date=rng.maybe(random_day(rng)),
-                reference=rng.choice("test", ""),
-                external_reference=rng.maybe(rng.choice("test", "")),
-                payment=rng.uniform(0, 100),
-                vat=rng.maybe(rng.uniform(0, 100)),
-                invoice_type=rng.choice("Mike", "", None),
-                note=rng.choice("Test invoice", "", None),
-            )
+            invoice = self.__random_invoice(rng, paid_date=rng.maybe(random_day(rng)))
             db.add_invoice(invoice)
             invoices = list(db.get_invoices(first_issue_date=rng.maybe(invoice.issue_date), last_issue_date=rng.maybe(invoice.issue_date)))
             self.assertEqual(len(invoices), 1)
             self.assertEqual(invoices[0], invoice)
+
+    @RandomisedTest(number_of_runs=20)
+    def test_update_invoice(self, rng):
+        with TemporaryDirectory() as tmpdir:
+            db = VortexSqlite3DB(Path(tmpdir) / "test.db")
+            invoice = self.__random_invoice(rng, paid_date=None)
+            db.add_invoice(invoice)
+            invoices = list(db.get_invoices(first_issue_date=rng.maybe(invoice.issue_date), last_issue_date=rng.maybe(invoice.issue_date)))
+            self.assertEqual(len(invoices), 1)
+            self.assertEqual(invoices[0], invoice)
+
+            invoice2 = KashflowInvoice(
+                invoice.issue_date,
+                random_day(rng),
+                invoice.reference,
+                invoice.external_reference,
+                invoice.payment,
+                invoice.vat,
+                invoice.invoice_type,
+                invoice.note,
+            )
+            db.update_invoice(invoice2)
+            invoices = list(db.get_invoices(first_issue_date=rng.maybe(invoice.issue_date), last_issue_date=rng.maybe(invoice.issue_date)))
+            self.assertEqual(len(invoices), 1)
+            self.assertEqual(invoices[0], invoice2)
--- sqlite3_db/vortex_sqlite3_db.py
+++ sqlite3_db/vortex_sqlite3_db.py
@@ -5,6 +5,7 @@ from typing import Optional
 from date_range import Day
 from env import VORTEX_DB_PATH
 from kashflow.invoice import KashflowInvoice
+from sqlite3_db.sqlite3_context import Sqlite3
 from utils import checked_type
 
 
@@ -16,64 +17,87 @@ class VortexSqlite3DB:
             self._create_db()
 
     def _create_db(self):
-        con = sqlite3.connect(str(self.path))
-        cur = con.cursor()
-        cur.execute("""
-        CREATE TABLE kashflow (
-            id INTEGER PRIMARY KEY,
-            issue_date TEXT NOT NULL,
-            paid_date TEXT,
-            reference TEXT not null,
-            external_reference TEXT,
-            payment real not null,
-            vat real,
-            type TEXT,
-            note TEXT
-        )
-        """)
+        with Sqlite3(str(self.path)) as cur:
+            cur.execute("""
+            CREATE TABLE kashflow (
+                id INTEGER PRIMARY KEY,
+                issue_date TEXT NOT NULL,
+                paid_date TEXT,
+                reference TEXT not null,
+                external_reference TEXT,
+                payment real not null,
+                vat real,
+                type TEXT,
+                note TEXT
+            )
+            """)
 
     def add_invoice(
             self,
             invoice: KashflowInvoice
     ):
-        con = sqlite3.connect(str(self.path))
-        cur = con.cursor()
-        cur.execute("""
-        INSERT INTO kashflow (issue_date, paid_date, reference, external_reference, payment, vat, type, note)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        """, (
-            invoice.issue_date.iso_repr,
-            invoice.paid_date.iso_repr if invoice.paid_date else None,
-            invoice.reference,
-            invoice.external_reference,
-            invoice.payment,
-            invoice.vat,
-            invoice.invoice_type,
-            invoice.note))
-        con.commit()
-        con.close()
+        with Sqlite3(str(self.path)) as cur:
+            cur.execute("""
+            INSERT INTO kashflow (issue_date, paid_date, reference, external_reference, payment, vat, type, note)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """, (
+                invoice.issue_date.iso_repr,
+                invoice.paid_date.iso_repr if invoice.paid_date else None,
+                invoice.reference,
+                invoice.external_reference,
+                invoice.payment,
+                invoice.vat,
+                invoice.invoice_type,
+                invoice.note))
 
     def get_invoices(self, first_issue_date: Optional[Day], last_issue_date: Optional[Day]) -> list[KashflowInvoice]:
-        con = sqlite3.connect(str(self.path))
-        cur = con.cursor()
-        first_issue_date = first_issue_date or Day(1970, 1, 1)
-        last_issue_date = last_issue_date or Day(9999, 12, 31)
-        res = cur.execute("""
-        SELECT issue_date, paid_date, reference, external_reference, payment, vat, type, note
-        FROM kashflow
-        WHERE issue_date >= ? AND issue_date <= ?
-        """, (first_issue_date.iso_repr, last_issue_date.iso_repr))
-        rows = res.fetchall()
-        for row in rows:
-            yield KashflowInvoice(
-                issue_date=Day.parse(row[0]),
-                paid_date=Day.parse(row[1]) if row[1] is not None else None,
-                reference=row[2],
-                external_reference=row[3],
-                payment=row[4],
-                vat=row[5],
-                invoice_type=row[6],
-                note=row[7]
-            )
+        with Sqlite3(str(self.path)) as cur:
+            first_issue_date = first_issue_date or Day(1970, 1, 1)
+            last_issue_date = last_issue_date or Day(9999, 12, 31)
+            res = cur.execute("""
+            SELECT issue_date, paid_date, reference, external_reference, payment, vat, type, note
+            FROM kashflow
+            WHERE issue_date >= ? AND issue_date <= ?
+            """, (first_issue_date.iso_repr, last_issue_date.iso_repr))
+            rows = res.fetchall()
+            for row in rows:
+                yield KashflowInvoice(
+                    issue_date=Day.parse(row[0]),
+                    paid_date=Day.parse(row[1]) if row[1] is not None else None,
+                    reference=row[2],
+                    external_reference=row[3],
+                    payment=row[4],
+                    vat=row[5],
+                    invoice_type=row[6],
+                    note=row[7]
+                )
 
+    def has_invoice(self, invoice: KashflowInvoice) -> bool:
+        with Sqlite3(str(self.path)) as cur:
+            res = cur.execute("""
+            select count(*) FROM kashflow
+            WHERE reference = ?
+            and issue_date = ?
+            """, (
+                invoice.reference,
+                invoice.issue_date.iso_repr,
+            ))
+            num_rows = res.fetchone()[0]
+            assert num_rows <= 1, f"Expected 0 or 1 rows, got {num_rows}"
+            return num_rows == 1
 
+    def delete_invoice(self, invoice: KashflowInvoice):
+        with Sqlite3(str(self.path)) as cur:
+            cur.execute("""
+            DELETE FROM kashflow
+            WHERE reference = ?
+            and issue_date = ?
+            """, (
+                invoice.reference,
+                invoice.issue_date.iso_repr,
+            ))
+
+    def update_invoice(self, invoice: KashflowInvoice):
+        if self.has_invoice(invoice):
+            self.delete_invoice(invoice)
+        self.add_invoice(invoice)
